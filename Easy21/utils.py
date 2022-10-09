43c7e08c339268449e0b6df9e600855d987bbdbf
yoaverez <yoav.erez.01@gmail.com>
2022-10-09 17:39:33 +0300

    add 2d plot

--- Easy21/utils.py
+++ Easy21/utils.py
@@ -60,3 +60,76 @@ def plot_3D(x, y, z, title, xlabel='Dealer showing', ylabel='Player sum', zlabel
     if not is_2_plots or z.ndim == 3:
         fig.colorbar(surf, shrink=0.5, aspect=8, pad=0.1)
     plt.show()
+
+
+def plot_2D(x, ys, ys_ids, font_size=16, fig_size=(10, 8), same_plot=True, x_label='', y_label='',
+            title='', ys_colors=None, culomns=1, x_labels=[], y_labels=[], multiple_curves=False, curve_labels=[],
+            save_fig_path=''):
+    """
+    print multiple curves on a single plot or multiple subplots
+
+    :param x: The x_axis points. if same_plot=False than it's should be a list of x_axis points.
+    Each list is x_axis points for one subplot.
+    :param ys: List in which each element is a list of y_axis points.
+    :param ys_ids: :List of names for each plot. if same_plot=True than it will appear as legends else
+    as a title to specific subplot.
+    :param font_size: The font size of the title and legends. Default is 16.
+    In subplots the recommended font size is 10.
+    :param fig_size: Adjust the size of the plot or each subplot.
+
+
+    :param same_plot: If True, we plot all the ys curves on a single plot. Else plot each element of
+    (x,y) in zip(x,ys) as a different subplot. Default is True.
+    :param x_label: Used only if same_plot=True and is the title of the x_axis.
+    :param y_label: Used only if same_plot=True and is the title of the y_axis.
+    :param title: Used only if same_plot=True. The plot title.
+    :param ys_colors: Used only if same_plot=True. List of colors (e.g. 'b', 'r') for each curve.
+
+    :param culomns: Used only if same_plot=False. The number of subplots in each row. Default to 1.
+    Deafult to None, meaning choose automatic colors.
+    :param x_labels: Used only if same_plot=False. List of names for each subplot x_axis.
+    :param y_labels: Used only if same_plot=False. List of names for each subplot y_axis.
+    :param multiple_curves: Used only if same_plot=False. If true, each subplot will have multiple curves
+    else, each subplot will have a single curve. Default is False.
+    :param curve_labels: Used only if same_plot=False and multiple_curves=True. Contaions labels for each curve in each subplot.
+    It's shape should be the same as ys shape.
+    """
+    if same_plot:
+        fig, ax = plt.subplots(1, 1, figsize=fig_size, )
+        plt.rcParams['font.size'] = f'{font_size}'
+        for i, (y, y_id) in enumerate(zip(ys, ys_ids)):
+            if ys_colors is None:
+                ax.plot(x, y, label=y_id, marker='*')
+            else:
+                ax.plot(x, y, label=y_id, color=ys_colors[i], marker='*')
+
+        ax.set_xlabel(x_label, fontsize=font_size)
+        ax.set_ylabel(y_label, fontsize=font_size)
+        ax.grid()
+        ax.legend()
+        ax.set_title(title)
+    else:
+        rows = len(ys) // culomns + 1
+
+        fig, ax = plt.subplots(rows, culomns, figsize=fig_size, )
+        plt.rcParams['font.size'] = f'{font_size}'
+        for i in range(rows):
+            for j in range(culomns):
+                index = i * culomns + j
+                if index >= len(ys):
+                    ax[i, j].remove()
+                    continue
+                if multiple_curves:
+                    for curve, curve_label in zip(ys[index], curve_labels[index]):
+                        ax[i][j].plot(x[index], curve, label=curve_label, marker='*')
+                    ax[i][j].legend()
+                else:
+                    ax[i][j].plot(x[index], ys[index])
+                ax[i][j].set_xlabel(x_labels[index], fontsize=font_size)
+                ax[i][j].set_ylabel(y_labels[index], fontsize=font_size)
+                ax[i][j].set_title(ys_ids[index])
+                ax[i][j].grid()
+    fig.tight_layout()
+    if save_fig_path != '':
+        plt.savefig(save_fig_path)
+    plt.show()
