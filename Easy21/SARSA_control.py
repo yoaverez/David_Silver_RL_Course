3f46dc50ba60b22ef689061b1dd6d3df4cb16cab
yoaverez <yoav.erez.01@gmail.com>
2022-10-09 13:00:48 +0300

    create a base class for the different agents

---
+++ Easy21/SARSA_control.py
@@ -0,0 +1,87 @@
+from utils import *
+
+from agent import Agent
+
+import numpy as np
+import random
+
+
+class SarsaControlAgent(Agent):
+
+    def __init__(self, N0=100, discount=1, lambda_=0, log_path=''):
+
+        super().__init__(N0, log_path)
+
+        self.gamma = discount
+        self.lambda_ = lambda_
+
+        self.eligibility_traces = np.zeros_like(self.N)
+
+    def train_agent(self, episodes, **kwargs):
+        count_wins, count_loss, count_tie = 0, 0, 0
+        if self.log:
+            print(f"logging to {self.log_path}")
+        for episode in range(1, episodes+1):
+            print(f"running episode {episode}/{episodes}...")
+            # sampling stage:
+            self.reset_eligibility_traces()
+            state = self.env.reset()
+            action, int_action = self.eps_greedy_action(state)
+            while not self.env.done:
+                new_state, reward, log_str = self.env.step(state, action)
+
+                i1, j1 = state
+                i2, j2 = new_state
+
+                self.N[i1, j1, int_action] += 1
+
+                new_action, new_int_action = self.eps_greedy_action(new_state)
+
+                q_prediction = reward
+                if not self.env.done:
+                    q_prediction += self.gamma * self.Q[i2, j2, new_int_action]
+
+                delta_t = q_prediction - self.Q[i1, j1, int_action]
+                self.eligibility_traces[i1, j1, int_action] += 1
+
+                alpha_t = 1 / self.N[i1, j1, int_action]
+                # update all the action-value function elements
+                # and all the eligibility_traces elements
+
+                # for i, rows in enumerate(self.eligibility_traces):
+                #     for j, cols in enumerate(rows):
+                #         for a, _ in enumerate(cols):
+                #             self.Q[i, j, a] += alpha_t*delta_t*self.eligibility_traces[i, j, a]
+                #             self.eligibility_traces[i, j, a] *= self.gamma * self.lambda_
+
+                # alternative more fast update
+                self.Q += alpha_t*delta_t*self.eligibility_traces
+                self.eligibility_traces *= self.gamma * self.lambda_
+
+                # update log
+                if self.log and episodes - episode <= 100:
+                    with open(self.log_path, 'a', newline='\n') as f:
+                        s = f"dealer open card: {state[0]:3} {' ':3}{'|':3} player sum: {state[1]:3} {' ':3}{'|':3} " \
+                            f"action: {action:6} {' ':3}{'|':3} reward: {reward:3}\n"
+                        f.write(s)
+                        f.write(log_str)
+
+                state = new_state
+                action, int_action = new_action, new_int_action
+
+            if reward > 0:
+                count_wins += 1
+            elif reward < 0:
+                count_loss += 1
+            else:
+                count_tie += 1
+        if self.log:
+            with open(self.log_path, 'a', newline='\n') as f:
+                f.write(f"player won {count_wins} games\n"
+                        f"player lost {count_loss} games\n"
+                        f"{count_tie} games ended with a tie\n")
+
+        print(self.Q)
+
+    def reset_eligibility_traces(self):
+        self.eligibility_traces = np.zeros_like(self.N)
---
+++ Easy21/agent.py
@@ -0,0 +1,60 @@
+from utils import *
+from environment import Easy21
+import random
+import numpy as np
+
+
+class Agent:
+    def __init__(self, N0=100, log_path=''):
+        self.log = False if log_path == '' else True
+        self.log_path = log_path
+        if self.log:
+            with open(self.log_path, 'w', newline='\n') as f:
+                # clean file
+                pass
+        self.env = Easy21()
+        self.N0 = N0
+        self.N = np.zeros(shape=(CARDS_NUM + 1, LEGAL_SUM_FIELD + 1, NUM_OF_ACTIONS))  # (state, action)
+        self.Q = np.zeros_like(self.N)
+
+    @staticmethod
+    def calc_value_func_from_action_value_func(Q):
+        n_d1, n_d2, n_d3 = Q.shape
+        V = np.amax(Q, axis=2)
+        # sanity check:
+        assert V.shape == (n_d1, n_d2)
+        return V
+
+    def eps_greedy_action(self, state):
+        if self.env.done:
+            return "not_important", -1
+        i, j = state
+        N_state = np.sum(self.N[i, j])
+        eps_t = self.N0/(self.N0 + N_state)
+        if random.random() < eps_t:
+            # pick random action
+            if random.random() < 0.5:
+                return "hit", ACTION_SPACE.actions_to_int["hit"]
+            else:
+                return "stick", ACTION_SPACE.actions_to_int["stick"]
+        else:
+            # pick greedy action
+            int_action = np.argmax(self.Q[i, j])
+            return ACTION_SPACE.int_to_actions[int_action], int_action
+
+    def train_agent(self, episodes, **kwargs):
+        raise Exception("VIRTUAL FUNCTION NOT IMPLEMENTED")
+
+    def plot_value_function(self, title):
+        x = np.arange(0, 10, 1)
+        y = np.arange(0, 21, 1)
+        z = self.calc_value_func_from_action_value_func(self.Q)[1:, 1:].transpose()
+
+        plot_3D(x, y, z, title=title)
+
+    def plot_action_value_function(self, title, single_plot=False):
+        x = np.arange(0, 10, 1)
+        y = np.arange(0, 21, 1)
+        z = np.transpose(self.Q[1:, 1:], axes=(1, 0, 2))
+
+        plot_3D(x, y, z, title=title, zlabel="Q(s,a)", titles=["Q(s,a=hit)", "Q(s,a=stick)"], single_plot=single_plot)
